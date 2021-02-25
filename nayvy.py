82d3a9db563192f01fed813d56b0ae3c9b7dd5e1
vsedov <viv.sedov@hotmail.com>
2021-02-25 15:10:27 +0000

    Nayvy config / python setup added

---
+++ nayvy.py
@@ -0,0 +1,357 @@
+from pprintpp import pprint as pp
+import abc
+import aifc
+import argparse
+import array
+import ast
+import asynchat
+import asyncio
+import asyncore
+import atexit
+import audioop
+import base64
+import bdb
+import binascii
+import binhex
+import bisect
+import builtins
+import bz2
+import calendar
+import cgi
+import cgitb
+import chunk
+import cmath
+import cmd
+import code
+import codecs
+import codeop
+import collections
+import collections.abc
+import colorsys
+import compileall
+import concurrent.futures
+import configparser
+import contextlib
+import copy
+import copyreg
+import cProfile
+import crypt
+import csv
+import ctypes
+import curses
+import curses.ascii
+import curses.panel
+import curses.textpad
+import dataclasses
+import datetime
+import dbm
+import dbm.dumb
+import dbm.gnu
+import dbm.ndbm
+import decimal
+import difflib
+import dis
+import distutils
+import distutils.archive_util
+import distutils.bcppcompiler
+import distutils.ccompiler
+import distutils.cmd
+import distutils.command
+import distutils.command.bdist
+import distutils.command.bdist_dumb
+import distutils.command.bdist_msi
+import distutils.command.bdist_packager
+import distutils.command.bdist_rpm
+import distutils.command.bdist_wininst
+import distutils.command.build
+import distutils.command.build_clib
+import distutils.command.build_ext
+import distutils.command.build_py
+import distutils.command.build_scripts
+import distutils.command.check
+import distutils.command.clean
+import distutils.command.config
+import distutils.command.install
+import distutils.command.install_data
+import distutils.command.install_headers
+import distutils.command.install_lib
+import distutils.command.install_scripts
+import distutils.command.register
+import distutils.command.sdist
+import distutils.core
+import distutils.cygwinccompiler
+import distutils.debug
+import distutils.dep_util
+import distutils.dir_util
+import distutils.dist
+import distutils.errors
+import distutils.extension
+import distutils.fancy_getopt
+import distutils.file_util
+import distutils.filelist
+import distutils.log
+import distutils.msvccompiler
+import distutils.spawn
+import distutils.sysconfig
+import distutils.text_file
+import distutils.unixccompiler
+import distutils.util
+import distutils.version
+import doctest
+import dummy_threading
+import email
+import email.charset
+import email.contentmanager
+import email.encoders
+import email.errors
+import email.generator
+import email.header
+import email.headerregistry
+import email.iterators
+import email.message
+import email.mime
+import email.parser
+import email.policy
+import email.utils
+import encodings.idna
+import encodings.mbcs
+import encodings.utf_8_sig
+import ensurepip
+import enum
+import errno
+import faulthandler
+import fcntl
+import filecmp
+import fileinput
+import fnmatch
+import formatter
+import fpectl
+import fractions
+import ftplib
+import functools as ft
+import gc
+import getopt
+import getpass
+import gettext
+import glob
+import grp
+import gzip
+import hashlib
+import heapq
+import hmac
+import html
+import html.entities
+import html.parser
+import http
+import http.client
+import http.cookiejar
+import http.cookies
+import http.server
+import imaplib
+import imghdr
+import imp
+import importlib
+import importlib.abc
+import importlib.machinery
+import importlib.util
+import inspect
+import io
+import ipaddress
+import itertools as it
+import json
+import json.tool
+import keyword
+import lib2to3
+import linecache
+import locale
+import logging
+import logging.config
+import logging.handlers
+import lzma
+import macpath
+import mailbox
+import mailcap
+import marshal
+import math
+import mimetypes
+import mmap
+import modulefinder
+import msilib
+import msvcrt
+import multiprocessing
+import multiprocessing.connection
+import multiprocessing.dummy
+import multiprocessing.managers
+import multiprocessing.pool
+import multiprocessing.sharedctypes
+import netrc
+import nis
+import nntplib
+import numbers
+import operator
+import optparse
+import os
+import os.path
+import ossaudiodev
+import parser
+import pathlib
+import pdb
+import pickle
+import pickletools
+import pipes
+import pkgutil
+import platform
+import plistlib
+import poplib
+import posix
+import profile
+import pstats
+import pty
+import pwd
+import py_compile
+import pyclbr
+import pydoc
+import queue
+import quopri
+import random
+import re
+import readline
+import reprlib
+import resource
+import rlcompleter
+import runpy
+import sched
+import secrets
+import select
+import selectors
+import shelve
+import shlex
+import shutil
+import signal
+import site
+import smtpd
+import smtplib
+import sndhdr
+import socket
+import socketserver
+import spwd
+import sqlite3
+import ssl
+import stat
+import statistics
+import string
+import stringprep
+import struct
+import subprocess
+import subprocess as sp
+import sunau
+import symbol
+import symtable
+import sys
+import sysconfig
+import syslog
+import tabnanny
+import tarfile
+import telnetlib
+import tempfile
+import termios
+import test
+import test.support
+import textwrap
+import threading
+import time
+import timeit
+import tkinter
+import tkinter.scrolledtext
+import tkinter.tix
+import tkinter.ttk
+import token
+import tokenize
+import trace
+import traceback
+import tracemalloc
+import tty
+import turtle
+import turtledemo
+import types
+import typing
+import unicodedata
+import unittest
+import unittest.mock
+import urllib
+import urllib.error
+import urllib.parse
+import urllib.request
+import urllib.response
+import urllib.robotparser
+import uu
+import uuid
+import venv
+import warnings
+import wave
+import weakref
+import webbrowser
+import winreg
+import winsound
+import wsgiref
+import wsgiref.handlers
+import wsgiref.headers
+import wsgiref.simple_server
+import wsgiref.util
+import wsgiref.validate
+import xdrlib
+import xml
+import xml.dom
+import xml.dom.minidom
+import xml.dom.pulldom
+import xml.etree.ElementTree
+import xml.parsers.expat
+import xml.parsers.expat.errors
+import xml.parsers.expat.model
+import xml.sax
+import xml.sax.handler
+import xml.sax.saxutils
+import xml.sax.xmlreader
+import xmlrpc.client
+import xmlrpc.server
+import zipapp
+import zipfile
+import zipimport
+import zlib
+from abc import ABCMeta, abstractclassmethod, abstractmethod
+from contextlib import contextmanager
+from dataclasses import dataclass
+from enum import Enum, IntEnum, auto, unique
+from functools import reduce
+from itertools import (accumulate, chain, count, cycle, isslice, repeat,
+                       starmap, tee, zip_longest)
+
+from os.path import abspath, basename, dirname, exists, relpath
+from pathlib import Path
+from pprint import pprint as p
+from typing import (TYPE_CHECKING, AbstractSet, Any, AnyStr,
+                    AsyncContextManager, AsyncGenerator, AsyncIterable,
+                    AsyncIterator, Awaitable, ByteString, Callable, ChainMap,
+                    ClassVar, Collection, Container, ContextManager, Coroutine,
+                    Counter, DefaultDict, Deque, Dict, FrozenSet, Generator,
+                    Generic, Hashable, ItemsView, Iterable, Iterator, KeysView,
+                    List, Mapping, MappingView, MutableMapping,
+                    MutableSequence, MutableSet, NamedTuple, NewType, NoReturn,
+                    Optional, OrderedDict, Reversible, Sequence, Set, Sized,
+                    SupportsAbs, SupportsBytes, SupportsComplex, SupportsFloat,
+                    SupportsInt, SupportsRound, Text, Tuple, Type, TypeVar,
+                    Union, ValuesView, cast, get_type_hints, no_type_check,
+                    no_type_check_decorator, overload)
+
+import click
+import numba
+import numpy as np
+import pandas as pd
+import pytest
+import requests
+import seaborn as sns
+import tensorflow as tf
+import torch
+import yaml
+from logzero import logger
+from pytest import fixture
