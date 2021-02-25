614fcf08def554bcc1c468122c82c3076c37bb04
vsedov <viv.sedov@hotmail.com>
2021-02-25 13:55:52 +0000

    Local COnfig , profile , xint etc added

---
+++ dot_fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/viv/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/viv/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/viv/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/viv/.fzf/shell/key-bindings.zsh"
