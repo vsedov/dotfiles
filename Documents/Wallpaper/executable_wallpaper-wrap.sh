fa242e23ee0a6ffb0abdfee75e36238d80aa6ded
vsedov <viv.sedov@hotmail.com>
2021-02-25 14:54:59 +0000

    xrandr stuff added

---
+++ Documents/Wallpaper/executable_wallpaper-wrap.sh
@@ -0,0 +1,6 @@
+xwinwrap -g 1920x1080+1920+0 -ov -ni -- mpv --hwdec=auto-copy --vo=gpu --no-border --no-config --no-window-dragging  --no-osd-bar --no-sub  --loop -wid WID --no-audio -quiet /home/viv/Downloads/Main21.mp4 -loop 0 &
+
+xwinwrap -g 1920x1080 -ov -ni -- mpv --hwdec=auto-copy --vo=gpu --no-border --no-config --no-window-dragging  --no-osd-bar --no-sub  --loop -wid WID --no-audio -quiet /home/viv/Downloads/Main5.mp4 -loop 0 &
+
+
+
